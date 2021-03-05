3b26874acaa68f3f3618f8bd52372b7afe2c418f
lalitsecret <lalitsecret@gmail.com>
2021-03-05 13:57:03 +0530

    1

---
+++ src/redux/data.js
@@ -0,0 +1,320 @@
+let data={
+  "admin": [
+    {
+      "name": "admin",
+      "email": "admin@gmail.com",
+      "phone": "56789",
+      "password": "admin",
+      "id": 1
+    },
+    {
+      "name": "su",
+      "email": "su@gmail.com",
+      "phone": "345434543",
+      "password": "su@123",
+      "id": 2
+    }
+  ],
+  "faculty": [
+    {
+      "name": "lalit",
+      "email": "lalit@gmail.com",
+      "phone": "4567890",
+      "password": "lalit",
+      "branch": "cse",
+      "id": 1
+    },
+    {
+      "name": "amit",
+      "email": "amit@gmail.com",
+      "phone": "8767878787",
+      "password": "amit",
+      "branch": "cse",
+      "id": 2
+    },
+    {
+      "name": "sumit",
+      "email": "sumit@gmail.com",
+      "phone": "2232323233",
+      "password": "sumit",
+      "branch": "cse",
+      "id": 3
+    },
+    {
+      "name": "punit",
+      "email": "punit@gmail.com",
+      "phone": "9898798798787",
+      "password": "punit",
+      "branch": "it",
+      "id": 4
+    }
+  ],
+  "student": [
+    {
+      "name": "rihabh",
+      "email": "rihabh@gmail.com",
+      "phone": "234567890",
+      "password": "rihabh",
+      "cid": "react",
+      "id": 1
+    },
+    {
+      "name": "himanshu",
+      "email": "himanshu@gmail.com",
+      "phone": "1254215412",
+      "password": "himanshu",
+      "cid": "react",
+      "id": 2
+    },
+    {
+      "name": "pradeep",
+      "email": "pradeep@yahoo.com",
+      "phone": "12312312323",
+      "password": "pradeep",
+      "cid": "mern stack",
+      "id": 3
+    }
+  ],
+  "courses": [
+    {
+      "name": "react",
+      "price": "12000",
+      "days": "45",
+      "id": 1
+    },
+    {
+      "name": "angular",
+      "price": "12000",
+      "days": "45",
+      "id": 2
+    },
+    {
+      "name": "mern stack",
+      "price": "356000",
+      "days": "45",
+      "id": 3
+    },
+    {
+      "name": "meanstack",
+      "price": "356000",
+      "days": "45",
+      "id": 4
+    }
+  ],
+  "slots": [
+    {
+      "name": "7-8",
+      "id": 1
+    },
+    {
+      "name": "8-9",
+      "id": 2
+    },
+    {
+      "name": "9-10",
+      "id": 3
+    },
+    {
+      "name": "10-11",
+      "id": 4
+    },
+    {
+      "name": "11-12",
+      "id": 5
+    },
+    {
+      "name": "12-1",
+      "id": 6
+    },
+    {
+      "name": "1-2",
+      "id": 7
+    },
+    {
+      "name": "2-3",
+      "id": 8
+    },
+    {
+      "name": "3-4",
+      "id": 9
+    },
+    {
+      "name": "4-5",
+      "id": 10
+    },
+    {
+      "name": "305 saturday",
+      "id": 11
+    }
+  ],
+  "batch": [
+    {
+      "p": 0,
+      "reason": "",
+      "remarks": "",
+      "approved": "",
+      "fid": "lalit",
+      "sid": "rihabh",
+      "cid": "react",
+      "slot_id": "12-1",
+      "dt": "2021-03-06",
+      "id": 1
+    },
+    {
+      "p": 0,
+      "reason": "",
+      "remarks": "",
+      "approved": "",
+      "fid": "lalit",
+      "sid": "rihabh",
+      "cid": "react",
+      "slot_id": "1-2",
+      "dt": "2021-03-06",
+      "id": 2
+    },
+    {
+      "p": 0,
+      "reason": "",
+      "remarks": "",
+      "approved": "",
+      "fid": "lalit",
+      "sid": "rihabh",
+      "cid": "react",
+      "slot_id": "2-3",
+      "dt": "2021-03-06",
+      "id": 3
+    },
+    {
+      "p": 0,
+      "reason": "",
+      "remarks": "",
+      "approved": "",
+      "fid": "amit",
+      "sid": "rihabh",
+      "cid": "react",
+      "slot_id": "7-8",
+      "dt": "2021-03-06",
+      "id": 4
+    },
+    {
+      "p": 0,
+      "reason": "",
+      "remarks": "",
+      "approved": "",
+      "fid": "amit",
+      "sid": "rihabh",
+      "cid": "react",
+      "slot_id": "8-9",
+      "dt": "2021-03-06",
+      "id": 5
+    },
+    {
+      "p": 0,
+      "reason": "",
+      "remarks": "",
+      "approved": "",
+      "fid": "amit",
+      "sid": "rihabh",
+      "cid": "react",
+      "slot_id": "9-10",
+      "dt": "2021-03-06",
+      "id": 6
+    },
+    {
+      "p": 0,
+      "reason": "",
+      "remarks": "",
+      "approved": "",
+      "fid": "amit",
+      "sid": "rihabh",
+      "cid": "react",
+      "slot_id": "10-11",
+      "dt": "2021-03-06",
+      "id": 7
+    },
+    {
+      "p": 0,
+      "reason": "",
+      "remarks": "",
+      "approved": "",
+      "fid": "sumit",
+      "sid": "pradeep",
+      "cid": "mern stack",
+      "slot_id": "7-8",
+      "dt": "2021-03-06",
+      "id": 8
+    },
+    {
+      "p": 0,
+      "reason": "",
+      "remarks": "",
+      "approved": "",
+      "fid": "sumit",
+      "sid": "pradeep",
+      "cid": "mern stack",
+      "slot_id": "8-9",
+      "dt": "2021-03-06",
+      "id": 9
+    },
+    {
+      "p": 0,
+      "reason": "",
+      "remarks": "",
+      "approved": "",
+      "fid": "sumit",
+      "sid": "pradeep",
+      "cid": "mern stack",
+      "slot_id": "9-10",
+      "dt": "2021-03-06",
+      "id": 10
+    },
+    {
+      "p": 0,
+      "reason": "",
+      "remarks": "",
+      "approved": "",
+      "fid": "punit",
+      "sid": "rihabh",
+      "cid": "react",
+      "slot_id": "3-4",
+      "dt": "2021-03-06",
+      "id": 11
+    },
+    {
+      "p": 0,
+      "reason": "",
+      "remarks": "",
+      "approved": "",
+      "fid": "punit",
+      "sid": "rihabh",
+      "cid": "react",
+      "slot_id": "4-5",
+      "dt": "2021-03-06",
+      "id": 12
+    },
+    {
+      "p": 0,
+      "reason": "",
+      "remarks": "",
+      "approved": "",
+      "fid": "punit",
+      "sid": "himanshu",
+      "cid": "react",
+      "slot_id": "305 saturday",
+      "dt": "2021-03-06",
+      "id": 13
+    },
+    {
+      "p": 0,
+      "reason": "",
+      "remarks": "",
+      "approved": "",
+      "fid": "punit",
+      "sid": "himanshu",
+      "cid": "react",
+      "slot_id": "305 saturday",
+      "dt": "2021-03-06",
+      "id": 14
+    }
+  ]
+}
